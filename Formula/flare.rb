9503eb87f901f877388a49e0b03253a69e8bac78
Chris Hulbert <chris.hulbert@gmail.com>
2020-04-30 18:14:20 +1000

    1.2.1

--- Formula/flare.rb
+++ Formula/flare.rb
@@ -1,8 +1,8 @@
 class Flare < Formula
   desc "Flare: 2-way automagic sync to Backblaze B2"
   homepage "https://github.com/chrishulbert/flare"
-  url "https://github.com/chrishulbert/flare/releases/download/1.2/flare.zip"
-  sha256 "da5af128e763fa9802e9ad43d020e59e0844162fd17dff9a1a837fef05c6ec85"
+  url "https://github.com/chrishulbert/flare/releases/download/1.2.1/flare.zip"
+  sha256 "20fcfc89044b6b44c6317b960e214d4f3e637c1fd621860f8ddd44d842fe7d07"
   bottle :unneeded
 
   def install
